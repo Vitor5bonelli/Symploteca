abb453419e16747fa4807cc1b34b7ffb2af59d14
Vitor Bonelli <bonelli5.vitor@gmail.com>
2023-04-04 10:42:54 -0300

    chore: create domain and application directories

---
+++ src/main/java/org/example/application/main.java
@@ -0,0 +1,7 @@
+package org.example.application;
+
+public class main {
+    public static void main(String[] args) {
+        System.out.printf("OLá mundo");
+    }
+}
---
+++ src/main/java/org/example/domain/entities/book/Book.java
@@ -0,0 +1,11 @@
+package org.example.domain.entities.book;
+
+public class Book {
+    private Integer id;
+    private Integer edition;
+    private Integer numberOfPages;
+    private String name;
+    private String authors;
+    private String publisher;
+    private String isbn;
+}
